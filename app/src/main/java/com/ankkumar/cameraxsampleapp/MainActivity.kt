b56e1c09180a47b4ffe0504d1295aff15c533384
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-05-12 15:31:37 +0800

    adding permission code

--- app/src/main/java/com/ankkumar/cameraxsampleapp/MainActivity.kt
+++ app/src/main/java/com/ankkumar/cameraxsampleapp/MainActivity.kt
@@ -3,7 +3,20 @@ package com.ankkumar.cameraxsampleapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+
+import android.Manifest
+import android.util.Size
+import android.graphics.Matrix
+import androidx.lifecycle.LifecycleOwner
+import java.util.concurrent.TimeUnit
+
+
+private const val REQUEST_CODE_PERMISSIONS = 881
+
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+
+class MainActivity : AppCompatActivity(), LifecycleOwner {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
