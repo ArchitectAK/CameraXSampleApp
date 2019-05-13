32fba7861ef310e557190bb51d8cacde0c467d38
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-05-13 20:39:33 +0800

    created ImageAnalyzer

---
+++ app/src/main/java/com/ankkumar/cameraxsampleapp/ImageAnalyzer.kt
@@ -0,0 +1,28 @@
+package com.ankkumar.cameraxsampleapp
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+
+class ImageAnalyzer : ImageAnalysis.Analyzer {
+    override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+        val currentTimestamp = System.currentTimeMillis()
+        // Calculate the average luma no more often than every second
+        if (currentTimestamp - lastAnalyzedTimestamp >=
+            TimeUnit.SECONDS.toMillis(1)) {
+            // Since format in ImageAnalysis is YUV, image.planes[0]
+            // contains the Y (luminance) plane
+            val buffer = image.planes[0].buffer
+            // Extract image data from callback object
+            val data = buffer.toByteArray()
+            // Convert the data into an array of pixel values
+            val pixels = data.map { it.toInt() and 0xFF }
+            // Compute average luminance for the image
+            val luma = pixels.average()
+            // Log the new luma value
+            Log.d("CameraXApp", "Average luminosity: $luma")
+            // Update timestamp of last analyzed frame
+            lastAnalyzedTimestamp = currentTimestamp
+        }
+    }
+
+}
