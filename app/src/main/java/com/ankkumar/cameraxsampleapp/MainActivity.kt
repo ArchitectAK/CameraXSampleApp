09f20faddbd1ba1810a59705917dbdf4a9fa7b5f
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-05-13 20:21:48 +0800

    capture

--- app/src/main/java/com/ankkumar/cameraxsampleapp/MainActivity.kt
+++ app/src/main/java/com/ankkumar/cameraxsampleapp/MainActivity.kt
@@ -9,17 +9,18 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.util.Size
 import android.graphics.Matrix
+import android.util.Log
 import android.util.Rational
 import android.view.Surface
 import android.view.TextureView
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.Toast
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.util.concurrent.TimeUnit
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 
 private const val REQUEST_CODE_PERMISSIONS = 881
@@ -72,6 +73,40 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
 
             viewFinder.surfaceTexture = it.surfaceTexture
             updateTransform()
+        }
+
+
+        // Create configuration object for the image capture use case
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+                setTargetAspectRatio(Rational(1, 1))
+                // We don't set a resolution for image capture; instead, we
+                // select a capture mode which will infer the appropriate
+                // resolution based on aspect ration and requested mode
+                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+            }.build()
+
+        // Build the image capture use case and attach button click listener
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        capture_button.setOnClickListener {
+            val file = File(externalMediaDirs.first(),
+                "${System.currentTimeMillis()}.jpg")
+            imageCapture.takePicture(file,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(error: ImageCapture.UseCaseError,
+                                         message: String, exc: Throwable?) {
+                        val msg = "Photo capture failed: $message"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        Log.e("CameraX Sample App", msg)
+                        exc?.printStackTrace()
+                    }
+
+                    override fun onImageSaved(file: File) {
+                        val msg = "Photo capture succeeded: ${file.absolutePath}"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        Log.d("CameraX Sample App", msg)
+                    }
+                })
         }
 
         CameraX.bindToLifecycle(this, preview)
